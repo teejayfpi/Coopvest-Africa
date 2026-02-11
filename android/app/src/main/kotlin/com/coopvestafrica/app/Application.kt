806a75732a83fea670c29334fd1d68b228b1f739
Ayanlowololuwa1963 Olatunji <olatunjiayanlowo63@gmail.com>
2026-02-11 08:34:46 +0000

    Enable biometric authentication (fingerprint) for login

--- android/app/src/main/kotlin/com/coopvestafrica/app/Application.kt
+++ android/app/src/main/kotlin/com/coopvestafrica/app/Application.kt
@@ -1,9 +1,34 @@
 package com.coopvestafrica.app
 
 import android.app.Application
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.biometric.BiometricManager
 
-class Application : Application() {
+class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
+        
+        // Check biometric availability
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                // Biometric authentication is available
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                // No biometric hardware available
+            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                // Biometric hardware unavailable
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                // No biometrics enrolled
+            }
+        }
+    }
+
+    override fun registerWith(registry: PluginRegistry) {
+        GeneratedPluginRegistrant.registerWith(registry)
     }
 }
